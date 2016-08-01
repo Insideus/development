1d688ec537d953b39f11b1d88839d5ca33328a64
ftamburri <ftamburri@35b061ce-4e2e-4317-8f6e-9914f0c0709e>
2016-08-01 10:32:41 +0000

    Corrección para que arranque por el login

--- app/src/main/java/ar/com/fennoma/davipocket/activities/SplashActivity.java
+++ app/src/main/java/ar/com/fennoma/davipocket/activities/SplashActivity.java
@@ -41,8 +41,8 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void goToLogin() {
-        //Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        //Intent intent = new Intent(SplashActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
